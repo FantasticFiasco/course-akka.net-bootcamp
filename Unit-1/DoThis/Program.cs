fa6a1ee96b8ad0c24e8d8b1212ed183a7142aed5
Mattias Kindborg <FantasticFiasco@users.noreply.github.com>
2015-05-06 20:14:08 +0200

    Implemented lesson 2.

--- Unit-1/DoThis/Program.cs
+++ Unit-1/DoThis/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-﻿using Akka.Actor;
+﻿﻿using Akka.Actor;
 
 namespace WinTail
 {
-    #region Program
     class Program
     {
         public static ActorSystem MyActorSystem;
@@ -13,8 +11,6 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
-
             // time to make your first actors!
             IActorRef consoleWriterActor = MyActorSystem
                 .ActorOf(Props.Create(() => new ConsoleWriterActor()));
@@ -23,27 +19,10 @@ namespace WinTail
                 .ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
             
             // tell console reader to begin
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
         }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
-    #endregion
 }
